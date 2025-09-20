cba44834e783633e61ded6ba8bf46a3467d117ed
dev-nirob1 <dev.nirob1@gmail.com>
2025-09-20 23:29:12 +0600

    login-page

--- src/zems/Auth/Pages/LoginPage.vue
+++ src/zems/Auth/Pages/LoginPage.vue
@@ -1,3 +1,49 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
 <template>
-  login
+  <div class="login flex-center">
+    <div class="container">
+      <div class="medium-2 align-center gap-2">
+        <div class="p-1">
+          <BaseTitle class="text-center mb-2">Login To Your Account</BaseTitle>
+          <form>
+            <label>
+              Your Email
+              <InputField type="email" placeholder="Enter your email" required />
+            </label>
+            <label>
+              Your Password
+              <InputField type="password" placeholder="Enter your password" required />
+            </label>
+            <BaseButton class="width-full bg-secondary">Login</BaseButton>
+          </form>
+          <RouterLink class="text-right" to="">Forgotten Your Password</RouterLink>
+          <div class="text-center">
+            <SubTitle class="mt-3">Don't Have An Account</SubTitle>
+            <RouterLink to="/register">Register</RouterLink>
+
+          </div>
+        </div>
+        <div class="image">
+          <BaseImage image="/login.jpg" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.login {
+  min-height: 100vh;
+}
+.login .image {
+  width: 100%;
+  height: 100%;
+}
+.login .image img{
+  width: 100%;
+  height: 100%;
+}
+</style>
