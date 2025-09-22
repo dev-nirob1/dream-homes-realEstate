add60ee4b26d3e28c88493da281aaa86509760dd
dev-nirob1 <dev.nirob1@gmail.com>
2025-09-22 20:45:59 +0600

    update html of property feature

--- src/zems/Front/Components/Section/PropertyFeature.vue
+++ src/zems/Front/Components/Section/PropertyFeature.vue
@@ -1,95 +1,95 @@
 
 
 <template>
-  <div class="small-2 large-3 gap-2 mb-2">
-    <div class="property-feature">
+  <ul class="small-2 large-3 gap-2 mb-2">
+    <ListItem>
       <i class="fas fa-ruler-combined"></i>
       <div>
         <SubTitle>Land Size</SubTitle>
         <p>3.5</p>
       </div>
-    </div>
+    </ListItem>
 
-    <div class="property-feature">
+    <ListItem>
       <i class="fas fa-vector-square"></i>
       <div>
         <SubTitle>Flat Size</SubTitle>
         <p>1628</p>
       </div>
-    </div>
+    </ListItem>
 
-    <div class="property-feature">
+    <ListItem>
       <i class="fas fa-building"></i>
       <div>
         <SubTitle>Building Height</SubTitle>
         <p>G + 7</p>
       </div>
-    </div>
+    </ListItem>
 
-    <div class="property-feature">
+    <ListItem>
       <i class="fas fa-compass"></i>
       <div>
         <SubTitle>Facing</SubTitle>
         <p>North</p>
       </div>
-    </div>
+    </ListItem>
 
-    <div class="property-feature">
+    <ListItem>
       <i class="fas fa-car"></i>
       <div>
         <SubTitle>Parking</SubTitle>
         <p>6 Cars</p>
       </div>
-    </div>
+    </ListItem>
 
-    <div class="property-feature">
+    <ListItem>
       <i class="fas fa-elevator"></i> <!-- Font Awesome 6 has this -->
       <div>
         <SubTitle>Elevator Lift</SubTitle>
         <p>Yes</p>
       </div>
-    </div>
+    </ListItem>
 
-    <div class="property-feature">
+    <ListItem>
       <i class="fas fa-bolt"></i>
       <div>
         <SubTitle>Generator</SubTitle>
         <BaseParagraph>Yes</BaseParagraph>
       </div>
-    </div>
-    <div class="property-feature">
+    </ListItem>
+    <ListItem>
       <i class="fas fa-bolt"></i>
       <div>
         <SubTitle>Generator</SubTitle>
         <BaseParagraph>Yes</BaseParagraph>
       </div>
-    </div>
+    </ListItem>
 
-    <div class="property-feature">
+    <ListItem>
       <i class="fas fa-video"></i>
       <div>
         <SubTitle>Security</SubTitle>
         <BaseParagraph>CCTV Camera</BaseParagraph>
       </div>
-    </div>
-  </div>
+    </ListItem>
+  </ul>
 </template>
 
 <style scoped>
-.property-feature {
+ul li {
   display: flex;
   gap: .75rem;
   box-shadow: var(--box-shadow);
   padding: 1rem;
   border-radius: .5rem;
 }
-.property-feature .sub-title{
+li .sub-title{
   font-size: 1rem;
 }
-.property-feature p{
+li p{
   margin: .25rem 0 0 0;
 }
-.property-feature i{
+li i{
     font-size: 1.25rem;
     background: var(--secondary-color);
     color: var(--white-color);
