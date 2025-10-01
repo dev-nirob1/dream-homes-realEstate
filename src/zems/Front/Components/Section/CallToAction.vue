544831eac452b6526e0b4382d3aaaa7eef183914
dev-nirob1 <dev.nirob1@gmail.com>
2025-10-01 15:21:59 +0600

    update

--- src/zems/Front/Components/Section/CallToAction.vue
+++ src/zems/Front/Components/Section/CallToAction.vue
@@ -19,11 +19,9 @@
   color: var(--white-color);
   padding: 5rem 0;
   background:
-    linear-gradient(
-      rgb(from var(--dark-color) r g b / 15%),
-      rgb(from var(--dark-color) r g b / 15%)
-    ),
-    url('https://images.unsplash.com/photo-1549923746-c502d488b3ea?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') center / cover no-repeat;
+    linear-gradient(to right, rgb(from var(--dark-color) r g b / 15%),
+      rgb(from var(--primary-color) r g b / 10%)),
+    url('https://images.unsplash.com/photo-1544027993-37dbfe43562a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') center / cover no-repeat;
 }
 
 .call-to-action .btn {
